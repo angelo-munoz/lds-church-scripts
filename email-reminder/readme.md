826008bfab867ac21902e737ef216eced138b77e
Angelo Munoz <angelo250@gmail.com>
2021-03-06 09:33:16 -0600

    added email reminder script and starting readme

---
+++ email-reminder/readme.md
@@ -0,0 +1,5 @@
+# Send email reminders 
+Use this script to send email reminders using the reminder text pulled from a google sheet. 
+
+## Getting started
+1. Create a new google script
---
+++ email-reminder/script.js
@@ -0,0 +1,26 @@
+/**
+ * Sends emails with data from a google sheet. 
+ */
+function sendEmails() {
+    //get reminder spreadsheet
+    var sourceSheetId = "<your-google-sheet-id>", //the sheet Id with emails and messages
+        sourceRange = "A2:C50"; //the range of all topics
+
+    //get data 
+    const allData = Sheets.Spreadsheets.Values.get(sourceSheetId, sourceRange).values;
+
+    for (var i = 0; i < allData.length; i++) {
+        var recipient = allData[i][0], //email address
+            subject = allData[i][1], //message subject
+            body = allData[i][2]; //message body 
+
+        Logger.log(`recipient: %s, subject: %s, body: %s`, recipient, subject, body);
+
+        //send email
+        MailApp.sendEmail({
+            to: recipient,
+            subject: subject,
+            htmlBody: body
+        });
+    }
+}
